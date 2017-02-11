1a44f9936d8b5384e223204a166f2c3850c4f70c
jcaraballo <git@caraballo.eu>
2017-02-11 13:26:23 +0000

    Introduce scalacheck

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ version := "0.1"
 scalaVersion := "2.12.1"
 
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "3.0.1" % "test"
+  "org.scalatest" %% "scalatest" % "3.0.1" % "test",
+  "org.scalacheck" %% "scalacheck" % "1.13.4" % "test"
 )
--- src/test/scala/optics/IsoSpec.scala
+++ src/test/scala/optics/IsoSpec.scala
@@ -1,7 +1,10 @@
 package optics
 
+import org.scalacheck.Arbitrary.arbitrary
+import org.scalacheck.Gen
 import org.scalatest.FreeSpec
 import org.scalatest.Matchers._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
 case class FamilyName(text: String)
 object FamilyName {
@@ -19,21 +22,43 @@ object Entry {
     Iso[Entry, (String, String)](e => (e.name, e.value)){case (n, v) => Entry(n, v)}
 }
 
-class IsoSpec extends FreeSpec {
+class IsoSpec extends FreeSpec with GeneratorDrivenPropertyChecks {
+  private val familyNameGen: Gen[FamilyName] = for {
+    s ← arbitrary[String]
+  } yield FamilyName(s)
+
+  private val personGen: Gen[Person] = for {
+    fn ← familyNameGen
+  } yield Person(fn)
+
   "Iso" - {
-    "basic behaviour" in {
-      FamilyName.textI.get(FamilyName("Smith")) shouldBe "Smith"
-      FamilyName.textI.reverseGet("Smith") shouldBe FamilyName("Smith")
+    "basics" - {
+      "by example (FamilyName <-> String)" in {
+        FamilyName.textI.get(FamilyName("Smith")) shouldBe "Smith"
+        FamilyName.textI.reverseGet("Smith") shouldBe FamilyName("Smith")
+      }
+
+      "by example (Entry <-> tuple)" in {
+        Entry.asTupleI.get(Entry("id", "0021")) shouldBe ("id", "0021")
+        Entry.asTupleI.reverseGet(("id", "0021")) shouldBe Entry("id", "0021")
+      }
+
+      "property based" in {
+        forAll{(s: String) => FamilyName.textI.get(FamilyName(s)) shouldBe s}
+        forAll{(s: String) => FamilyName.textI.reverseGet(s) shouldBe FamilyName(s)}
+      }
     }
 
-    "tuple example" in {
-      Entry.asTupleI.get(Entry("id", "0021")) shouldBe ("id", "0021")
-      Entry.asTupleI.reverseGet(("id", "0021")) shouldBe Entry("id", "0021")
-    }
+    "properties" - {
+      "by example" in {
+        FamilyName.textI.reverseGet(FamilyName.textI.get(FamilyName("Smith"))) shouldBe FamilyName("Smith")
+        FamilyName.textI.get(FamilyName.textI.reverseGet("Smith")) shouldBe "Smith"
+      }
 
-    "basic properties" in {
-      FamilyName.textI.reverseGet(FamilyName.textI.get(FamilyName("Smith"))) shouldBe FamilyName("Smith")
-      FamilyName.textI.get(FamilyName.textI.reverseGet("Smith")) shouldBe "Smith"
+      "property based" in {
+        forAll(familyNameGen){ fn => FamilyName.textI.reverseGet(FamilyName.textI.get(fn)) shouldBe fn }
+        forAll{(s: String) => FamilyName.textI.get(FamilyName.textI.reverseGet(s)) shouldBe s}
+      }
     }
 
     "modify" in {
@@ -50,14 +75,21 @@ class IsoSpec extends FreeSpec {
       marriage(name) shouldBe FamilyName("Jones")
     }
 
-    "iso compose iso => iso" in {
+    "iso compose iso => iso" - {
       val composed: Iso[Person, String] = Person.familyNameI compose FamilyName.textI
 
-      val person = Person(FamilyName("Smith"))
-      composed.set("Jones")(person) shouldBe Person(FamilyName("Jones"))
+      "basics" in {
+        val person = Person(FamilyName("Smith"))
+        composed.set("Jones")(person) shouldBe Person(FamilyName("Jones"))
 
-      // as a comparison, how to do the same using each level's copy method
-      person.copy(familyName = person.familyName.copy(text = "Jones")) shouldBe Person(FamilyName("Jones"))
+        // as a comparison, how to do the same using each level's copy method
+        person.copy(familyName = person.familyName.copy(text = "Jones")) shouldBe Person(FamilyName("Jones"))
+      }
+
+      "composed satisfies properties" in {
+        forAll(personGen) { person => composed.reverseGet(composed.get(person)) shouldBe person }
+        forAll{(s: String) => composed.get(composed.reverseGet(s)) shouldBe s}
+      }
     }
   }
 }
